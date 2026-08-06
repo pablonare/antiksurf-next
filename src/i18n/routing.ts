d152537cb11f6cabf7edbf47c0042d1c94f11762
Pablo Naredo <pnaredobernardos@gmail.com>
2026-08-06 11:52:52 +0800

    intentado arreglar modal language

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,5 +3,6 @@ import {defineRouting} from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "es", "fr"],
   defaultLocale: "en",
-  localeDetection : false
+  localePrefix: "always",
+  localeDetection: false
 });
